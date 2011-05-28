ec023b1368bddf0dcee5bbe36921511c5c233aec
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-05-28 16:26:50 +0100

    base project added

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^admin/', include(admin.site.urls)),
+)

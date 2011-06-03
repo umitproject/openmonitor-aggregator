2517a6ce0a1dcd9c6ed3ecc0a38fbcad25faceb7
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-06-04 00:45:47 +0100

    Solved a typo in WebsiteSuggestion model

--- suggestions/models.py
+++ suggestions/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from messages.messages_pb2 import WebsiteSuggestion, ServiceSuggestion
 
 
 class WebsiteSuggestion(models.Model):
     websiteUrl = models.URLField(max_length=100)
     email      = models.EmailField(blank=True)
+
+    def create(websiteSuggestionMsg):
+        suggestion = WebsiteSuggestion()
+        suggestion.websiteUrl = websiteSuggestionMsg.websiteURL
+        suggestion.email = websiteSuggestionMsg.emailAddress
+        return suggestion
+
+    create = staticmethod(create)
 
 
 class ServiceSuggestion(models.Model):
@@ -11,5 +20,15 @@ class ServiceSuggestion(models.Model):
     hostName    = models.CharField(max_length=100)
     ip          = models.CharField(max_length=60)
     email       = models.EmailField(blank=True)
+
+    def create(serviceSuggestionMsg):
+        suggestion = ServiceSuggestion()
+        suggestion.serviceName = serviceSuggestionMsg.serviceName
+        suggestion.hostName = serviceSuggestionMsg.hostName
+        suggestion.ip = serviceSuggestionMsg.ip
+        suggestion.email = serviceSuggestionMsg.emailAddress
+        return suggestion
+
+    create = staticmethod(create)
     
 

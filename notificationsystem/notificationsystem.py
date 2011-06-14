6d41a94452c00b01bf3048f096292bd5592adebb
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-06-14 23:35:46 +0100

    Added notification system

--- notificationsystem/notificationsystem.py
+++ notificationsystem/notificationsystem.py
@@ -1,2 +1,27 @@
-__author__ = 'zeux'
-  
+from abc import ABCMeta
+
+
+class NotificationInterface:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def eventReceived(self, event):
+        return NotImplemented
+
+#NotificationInterface.register(Foo)
+
+
+class NotificationSystem:
+
+    def __init__(self):
+        self.subscribers = []
+
+    def registerSubscriber(self, subscriber):
+        if isinstance(subscriber, NotificationInterface):
+            self.subscribers.append(subscriber)
+        else:
+            raise TypeError, 'subscriber doesnt implement notification interface'
+
+    def publishEvent(self, event):
+        for subscriber in self.subscribers:
+            subscriber.eventReceived(event)

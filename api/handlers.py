faa8884606920a84f9e55a9565a4dfa1c8ce464a
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-06-16 22:54:56 +0100

    Solved problems in response message creation

--- api/handlers.py
+++ api/handlers.py
@@ -2,6 +2,7 @@ from piston.handler import BaseHandler
 from messages import messages_pb2
 from suggestions.models import WebsiteSuggestion, ServiceSuggestion
 from django.test.client import Client
+import logging
 import base64
 
 
@@ -9,6 +10,7 @@ class RegisterAgentHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("registerAgent received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedAgentRegister = messages_pb2.RegisterAgent()
@@ -23,8 +25,8 @@ class RegisterAgentHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.RegisterAgentResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         response.token = token
         response.privateKey = privateKey
         response.publicKey = publicKey
@@ -32,7 +34,7 @@ class RegisterAgentHandler(BaseHandler):
         response.cipheredPublicKey = cipheredPublicKey
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -40,6 +42,7 @@ class GetPeerListHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("getPeerList received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedMsg = messages_pb2.GetPeerList()
@@ -49,8 +52,8 @@ class GetPeerListHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.GetPeerListResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         knownPeer = response.knownPeers.add()
         knownPeer.token = "token"
         knownPeer.publicKey = "publickey"
@@ -59,7 +62,7 @@ class GetPeerListHandler(BaseHandler):
         knownPeer.agentPort = 50
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -67,6 +70,7 @@ class GetSuperPeerListHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("getSuperPeerList received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedMsg = messages_pb2.GetSuperPeerList()
@@ -76,8 +80,8 @@ class GetSuperPeerListHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.GetSuperPeerListResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         knownSuperPeer = response.knownSuperPeers.add()
         knownSuperPeer.token = "token"
         knownSuperPeer.publicKey = "publickey"
@@ -86,7 +90,7 @@ class GetSuperPeerListHandler(BaseHandler):
         knownSuperPeer.agentPort = 50
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -94,6 +98,7 @@ class GetEventsHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("getEvents received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedMsg = messages_pb2.GetEvents()
@@ -103,8 +108,8 @@ class GetEventsHandler(BaseHandler):
 
          # create the response
         response = messages_pb2.GetEventsResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         event = response.events.add()
         event.testType = "WEB"
         event.eventType = "CENSOR"
@@ -112,7 +117,7 @@ class GetEventsHandler(BaseHandler):
         event.sinceTimeUTC = 10
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -120,6 +125,7 @@ class SendWebsiteReportHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("sendWebsiteReport received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedWebsiteReport = messages_pb2.SendWebsiteReport()
@@ -129,11 +135,11 @@ class SendWebsiteReportHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.SendReportResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -141,6 +147,7 @@ class SendServiceReportHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("sendServiceReport received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedServiceReport = messages_pb2.SendServiceReport()
@@ -150,11 +157,11 @@ class SendServiceReportHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.SendReportResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -162,6 +169,7 @@ class CheckNewVersionHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("checkNewVersion received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedMsg = messages_pb2.NewVersion()
@@ -171,13 +179,13 @@ class CheckNewVersionHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.NewVersionResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         response.downloadURL = 2;
         response.versionNo = 4
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -185,6 +193,7 @@ class CheckNewTestHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("checkNewTest received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedMsg = messages_pb2.NewTests()
@@ -194,8 +203,8 @@ class CheckNewTestHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.NewTestsResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
         response.testVersionNo = 1
         test = response.tests.add()
         test.testID = 1;
@@ -203,7 +212,7 @@ class CheckNewTestHandler(BaseHandler):
         test.executeAtTimeUTC = 4000;
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -211,6 +220,7 @@ class WebsiteSuggestionHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("websiteSuggestion received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedWebsiteSuggestion = messages_pb2.WebsiteSuggestion()
@@ -222,11 +232,11 @@ class WebsiteSuggestionHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.TestSuggestionResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -234,6 +244,7 @@ class ServiceSuggestionHandler(BaseHandler):
     allowed_methods = ('POST',)
 
     def create(self, request):
+        logging.info("serviceSuggestion received")
         msg = base64.b64decode(request.POST['msg'])
 
         receivedServiceSuggestion = messages_pb2.ServiceSuggestion()
@@ -245,11 +256,11 @@ class ServiceSuggestionHandler(BaseHandler):
 
         # create the response
         response = messages_pb2.TestSuggestionResponse()
-        response.currentVersionNo = 1
-        response.currentTestVersionNo = 1
+        response.header.currentVersionNo = 1
+        response.header.currentTestVersionNo = 1
 
         # send back response
-        response_str = base64.b32encode(response.SerializeToString())
+        response_str = base64.b64encode(response.SerializeToString())
         return response_str
 
 
@@ -258,21 +269,30 @@ class TestsHandler(BaseHandler):
     allowed_methods = ('GET',)
 
     def read(self, request):
-        c = Client()
-        suggestion = messages_pb2.WebsiteSuggestion()
-        suggestion.header.token = "token"
-        suggestion.header.agentID = 5
-        suggestion.websiteURL = "www.example.com"
-        suggestion.emailAddress = "teste@domain.com"
-        sug_str = base64.b64encode(suggestion.SerializeToString())
-        response = c.post('/api/websitesuggestion/', {'msg': sug_str})
 
-        suggestion = messages_pb2.ServiceSuggestion()
-        suggestion.header.token = "token"
-        suggestion.header.agentID = 5
-        suggestion.serviceName = "p2p"
-        suggestion.emailAddress = "teste@domain.com"
-        suggestion.ip = "192.168.2.1"
-        suggestion.hostName = "newtestpc"
-        sug_str = base64.b64encode(suggestion.SerializeToString())
-        response = c.post('/api/servicesuggestion/', {'msg': sug_str})
+        try:
+            c = Client()
+            suggestion = messages_pb2.WebsiteSuggestion()
+            suggestion.header.token = "token"
+            suggestion.header.agentID = 5
+            suggestion.websiteURL = "www.example.com"
+            suggestion.emailAddress = "teste@domain.com"
+            sug_str = base64.b64encode(suggestion.SerializeToString())
+            response = c.post('/api/websitesuggestion/', {'msg': sug_str})
+        except Exception, inst:
+            logging.error(inst)
+
+        try:
+            suggestion = messages_pb2.ServiceSuggestion()
+            suggestion.header.token = "token"
+            suggestion.header.agentID = 5
+            suggestion.serviceName = "p2p"
+            suggestion.emailAddress = "teste@domain.com"
+            suggestion.ip = "192.168.2.1"
+            suggestion.hostName = "newtestpc"
+            sug_str = base64.b64encode(suggestion.SerializeToString())
+            response = c.post('/api/servicesuggestion/', {'msg': sug_str})
+        except Exception, inst:
+            logging.error(inst)
+
+        return 'test1'

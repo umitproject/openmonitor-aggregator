05b71c02cdae179ac8b199cdc64171d2e5a5b495
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-06-24 21:53:39 +0100

    More information added to events. Markercluster added to map

--- events/models.py
+++ events/models.py
@@ -1,21 +1,97 @@
 from django.db import models
+from django.utils import simplejson
 
+eventType = ["Censor", "Throttling", "Offline"]
+targetType = ["Website", "Service"]
+
+class EventType:
+    Censor, Throttling, Offline = range(3)
+
+    @staticmethod
+    def getEventType(id):
+        if id==EventType.Censor:
+            return "Censor"
+        elif id==EventType.Throttling:
+            return "Throttling"
+        elif id==EventType.Offline:
+            return "Offline"
+        else:
+            return "Unknown"
+
+class TargetType:
+    Website, Service = range(2)
+
+    @staticmethod
+    def getTargetType(id):
+        if id==TargetType.Website:
+            return "Website"
+        elif id==TargetType.Service:
+            return "Service"
+        else:
+            return "Unknown"
+    
 
 class Event(models.Model):
     targetType        = models.PositiveSmallIntegerField()
     eventType         = models.PositiveSmallIntegerField()
     firstDetectionUTC = models.DateTimeField()
     lastDetectionUTC  = models.DateTimeField()
-    activated         = models.BooleanField()    # indicate if the event is still happening
+    target            = models.TextField()
+    active            = models.BooleanField()    # indicate if the event is still happening
 
-    def getActiveEvents(self):
-        return self.activated==True
+    @staticmethod
+    def getActiveEvents(limit=0):
+        #return self.activated==True
+        # TODO: order by firstDetection or lastDetection ?
+        query = Event.objects.filter(active=True)#.order_by(lastDetectionUTC)
+        if limit>0:
+            query = query[:limit]
+        return query
+
+    # TODO: more search methods:
+    #   search by location (should be the name of place or coordinates ?
+    #   search by isp ?
+    #   search events for specific website
+    #   search events for specific services
+    #   search old events too, limited by dates
+
+    # get all the active events, in a location, and for a specific website/service
+    #@staticmethod
+    #def getActiveEvents(location, target):
+    #    pass
+
+
+    #def __unicode__(self):
+    #    return u'%s %s %s' % (self.targetType, self.eventType, self.firstDetectionUTC)
+
+    #def __str__(self):
+    #    return '%s %s %s' % (self.targetType, self.eventType, self.firstDetectionUTC)
+
+    def getDict(self):
+
+        locations = []
+        for location in self.eventlocation_set.all():
+            locations.append({'city': location.city, 'country': location.country, 'lat': location.latitude, 'lng': location.longitude})
+
+        event = {
+          'url': "/events/" + str(self.id),
+          'targetType': TargetType.getTargetType(self.targetType),
+          'target': self.target,
+          'type': EventType.getEventType(self.eventType),
+          'firstdetection': self.firstDetectionUTC.ctime(),
+          'lastdetection': self.lastDetectionUTC.ctime(),
+          'active': self.active,
+          'locations': locations
+        }
+        return event
 
 
 class EventLocation(models.Model):
     event    = models.ForeignKey('Event')
     city     = models.CharField(max_length=100)
     country  = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude= models.FloatField()
 
 
 class EventISP(models.Model):
--- gui/views.py
+++ gui/views.py
@@ -2,17 +2,58 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.utils import simplejson
 from google.appengine.api import channel
-
+from events.models import Event, EventType, EventLocation, TargetType
+import datetime
+from geopy import geocoders
 
 def map(request):
-    token = channel.create_channel('map')
-    return render_to_response('notificationsystem/map.html', {'token': token})
+    token = channel.create_channel('realtimebox')
+    events = Event.getActiveEvents()
+    events_dict = []
+    for event in events:
+        events_dict.append(event.getDict())
+    initialEvents = simplejson.dumps(events_dict)
+    return render_to_response('notificationsystem/map.html', {'token': token, 'initial_events': initialEvents})
 
 def realtimebox(request):
-    token = channel.create_channel('map')
-    return render_to_response('notificationsystem/realtimebox.html', {'token': token})
+    token = channel.create_channel('realtimebox')
+    events = Event.getActiveEvents()
+    events_dict = []
+    for event in events:
+        events_dict.append(event.getDict())
+    initialEvents = simplejson.dumps(events_dict)
+    return render_to_response('notificationsystem/realtimebox.html', {'token': token, 'initial_events': initialEvents})
 
 def send(request):
-    message = simplejson.dumps({'msg': 'This is a new event'})
-    channel.send_message('map', message)
+    g = geocoders.Google("ABQIAAAAWajWEMOvdMUzDSviTnY9_BQCULP4XOMyhPd8d_NrQQEO8sT8XBSloh91HGZYV6pQ4yQ1gkhp8E4bJw")
+    event = Event()
+    event.targetType = TargetType.Website
+    event.eventType = EventType.Offline
+    event.firstDetectionUTC = datetime.datetime(2011,06,24,11,00)
+    event.lastDetectionUTC = datetime.datetime(2011,06,24,11,32)
+    event.active = True
+    event.target = "www.twitter.com"
+    event.save()
+
+    location1 = EventLocation()
+    location1.city = "Aveiro"
+    location1.country = "Portugal"
+    location1.event = event
+    (place, point) = g.geocode(location1.city + ", " + location1.country)
+    location1.latitude = point[0]
+    location1.longitude = point[1]
+    location1.save()
+
+    location2 = EventLocation()
+    location2.city = "Leiria"
+    location2.country = "Portugal"
+    location2.event = event
+    (place, point) = g.geocode(location2.city + ", " + location2.country)
+    location2.latitude = point[0]
+    location2.longitude = point[1]
+    location2.save()
+    
+    message = simplejson.dumps(event.getDict())
+    channel.send_message('realtimebox', message)
+    #channel.send_message('map', message)
     return HttpResponse("Event sent")

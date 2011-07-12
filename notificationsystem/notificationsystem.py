ecec01e883136de2fd9496f99b14c2143983f81a
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-07-12 21:40:10 +0100

    Removed the use of abstract classes (not supported in gae). Implemented realtimeBox and realtimeMap notification system.

--- notificationsystem/notificationsystem.py
+++ notificationsystem/notificationsystem.py
@@ -1,27 +1,59 @@
-from abc import ABCMeta
+from django.utils import simplejson
+from google.appengine.api import channel
+import logging
 
-
+     
 class NotificationInterface:
-    __metaclass__ = ABCMeta
 
-    @abstractmethod
     def eventReceived(self, event):
         return NotImplemented
-
-#NotificationInterface.register(Foo)
 
 
 class NotificationSystem:
 
-    def __init__(self):
-        self.subscribers = []
+    subscribers = []
 
-    def registerSubscriber(self, subscriber):
+    def registerSubscriber(subscriber):
         if isinstance(subscriber, NotificationInterface):
-            self.subscribers.append(subscriber)
+            NotificationSystem.subscribers.append(subscriber)
         else:
             raise TypeError, 'subscriber doesnt implement notification interface'
 
-    def publishEvent(self, event):
-        for subscriber in self.subscribers:
-            subscriber.eventReceived(event)
+    def publishEvent(event):
+        logging.info(NotificationSystem.subscribers)
+        try:
+            for subscriber in NotificationSystem.subscribers:
+                subscriber.eventReceived(event)
+        except Exception,ex:
+            logging.error(ex)
+
+    registerSubscriber = staticmethod(registerSubscriber)
+    publishEvent = staticmethod(publishEvent)
+
+
+class RealtimeBox(NotificationInterface):
+
+    def eventReceived(self, event):
+        logging.info("event received on realtimebox")
+        try:
+            message = simplejson.dumps(event.getDict())
+            channel.send_message('realtimebox', message)
+        except Exception,ex:
+            logging.error(ex)
+
+
+class RealtimeMap(NotificationInterface):
+
+    def eventReceived(self, event):
+        logging.info("event received on realtimemap")
+        try:
+            message = simplejson.dumps(event.getDict())
+            channel.send_message('map', message)
+        except Exception,ex:
+            logging.error(ex)
+
+
+realtimeBox = RealtimeBox()
+realtimeMap = RealtimeMap()
+NotificationSystem.registerSubscriber(realtimeBox)
+NotificationSystem.registerSubscriber(realtimeMap)

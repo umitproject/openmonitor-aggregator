e6329a6b3925d24f77b8c917c695a323828045cd
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-08-23 22:58:13 +0100

    Added created_at field to suggetions

--- suggestions/models.py
+++ suggestions/models.py
@@ -3,16 +3,6 @@ from messages.messages_pb2 import WebsiteSuggestion, ServiceSuggestion
 
 
 class WebsiteSuggestion(models.Model):
-    # TODO (Adriano): We need a creation date. Otherwise, will be hard to tell
-    # if a given suggestion is current and still relevant. One of the
-    # parameters we can use to measure relevance is to calculate how many
-    # suggestions we got for the same website/service in a given amount of
-    # time (lets say 2 days). If we have 100 suggestions for www.twitter.com
-    # against 1 suggestion for www.nytimes.com then twitter is much more
-    # relevant than nytimes. But, if at another occasion we get some 50
-    # requests for nytimes in 2 days, then it suddenly becomes relevant.
-    # Remove my comments once you've read this.
-    # created_at = models.DateTimeField(auto_now_add=True)
     
     # TODO (Adriano): Another thing we need is to prioritize a region. Users
     # should be allowed to suggest a region to save up resources and make it
@@ -31,7 +21,8 @@ class WebsiteSuggestion(models.Model):
     # Remove my comments once you've read this and linked the list of
     # COUNTRIES to the choices.
     # region = models.CharField(max_length=50, choices=COUNTRIES)
-    
+
+    created_at = models.DateTimeField(auto_now_add=True)
     websiteUrl = models.URLField(max_length=100)
     email      = models.EmailField(blank=True)
 
@@ -49,16 +40,6 @@ class WebsiteSuggestion(models.Model):
 
 
 class ServiceSuggestion(models.Model):
-    # TODO (Adriano): We need a creation date. Otherwise, will be hard to tell
-    # if a given suggestion is current and still relevant. One of the
-    # parameters we can use to measure relevance is to calculate how many
-    # suggestions we got for the same website/service in a given amount of
-    # time (lets say 2 days). If we have 100 suggestions for www.twitter.com
-    # against 1 suggestion for www.nytimes.com then twitter is much more
-    # relevant than nytimes. But, if at another occasion we get some 50
-    # requests for nytimes in 2 days, then it suddenly becomes relevant.
-    # Remove my comments once you've read this.
-    # created_at = models.DateTimeField(auto_now_add=True)
     
     # TODO (Adriano): Another thing we need is to prioritize a region. Users
     # should be allowed to suggest a region to save up resources and make it
@@ -77,7 +58,8 @@ class ServiceSuggestion(models.Model):
     # Remove my comments once you've read this and linked the list of
     # COUNTRIES to the choices.
     # region = models.CharField(max_length=50, choices=COUNTRIES)
-    
+
+    created_at = models.DateTimeField(auto_now_add=True)
     serviceName = models.CharField(max_length=100)
     hostName    = models.CharField(max_length=100)
     ip          = models.CharField(max_length=60)
@@ -96,5 +78,3 @@ class ServiceSuggestion(models.Model):
         return self.serviceName
 
     create = staticmethod(create)
-    
-

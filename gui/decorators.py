958e5d3bf56ccd1cde5f7fdeef37d7968b7a3590
Adriano Marques <py.adriano@gmail.com>
2011-09-05 12:40:37 -0300

    More work on issue #494. Addthis is fully integrated and working now. In order to use, just call the appropriate tag from social templatetag

--- gui/decorators.py
+++ gui/decorators.py
@@ -19,5 +19,22 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-def send_event_tweet(event):
-    pass
+from django.contrib.auth.decorators import login_required as django_login_required
+from django.contrib.admin.views.decorators import staff_member_required as django_staff_member_required
+from django.conf import settings
+
+def login_required(view):
+    if not settings.DEBUG:
+        return django_login_required(view)
+    return view
+
+def staff_member_required(view):
+    if not settings.DEBUG:
+        def new_view(request, *args, **kwargs):
+            # This is in order to bypass authentication if this header is present,
+            # which indicates that appengine's cron is issuing this command
+            if settings.GAE and request.META.get("X-AppEngine-Cron", False) == "true":
+                return view(request, *args, **kwargs)
+            return django_staff_member_required(view)(request, *args, **kwargs)
+        return new_view
+    return view

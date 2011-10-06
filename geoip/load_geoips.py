6103b0da6d7da693e8e08c4a246cdeceebc97ec6
Adriano Marques <adriano@umitproject.org>
2011-10-06 09:58:49 -0700

    Ditched the geodata and old geoip lib. Moving everything into datastore, creating aggregations, caching and using data in a smarter way inside appengine. This revision is broken. Just won't work. Next revision will fix everything, but I wanted to do this intermediary commit to settle changes down before tackling the fixes.

---
+++ geoip/load_geoips.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##
+## Author: Adriano Monteiro Marques <adriano@umitproject.org>
+##
+## Copyright (C) 2011 S2S Network Consultoria e Tecnologia da Informacao LTDA
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+import urllib2
+import sys
+import zipfile
+import csv
+import json
+import urllib
+
+from os.path import join, dirname, exists
+
+CUR_DIR = dirname(__file__)
+GEO_FILE_ZIP = "GeoLiteCity_20110906.zip"
+GEO_EXTRACT_DIR = "GeoLiteCity_20110906"
+GEO_BLOCK_CSV = "GeoLiteCity_20110906/GeoLiteCity-Blocks.csv"
+GEO_LOCATION_CSV = "GeoLiteCity_20110906/GeoLiteCity-Location.csv"
+COUNTRY_CODE_CSV = "country_codes.csv"
+GEO_LITE = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/" + GEO_FILE_ZIP 
+BATCH_SIZE = 20
+SAVE_GEOIP_URL = "http://localhost:9000/geoip/save_geoip/"
+
+
+def downloader(url, fpath):
+    request = urllib2.Request(url)
+    _retriever(urllib2.urlopen(request), fpath)
+
+def _retriever(urlfile, fpath):
+    chunk = 4096
+    f = open(fpath, "w")
+    print "Downloading to %s" % fpath
+    while 1:
+        data = urlfile.read(chunk)
+        if not data:
+            print "Download to %s is done." % fpath
+            break
+        f.write(data)
+        print "Read %s bytes" % len(data)
+
+def download():
+    if exists(join(CUR_DIR, GEO_FILE_ZIP)):
+        if exists(join(CUR_DIR, GEO_BLOCK_CSV)) and exists(join(CUR_DIR, GEO_LOCATION_CSV)):
+            return
+    else:
+        downloader(GEO_LITE, GEO_FILE_ZIP)
+    
+    zip = zipfile.ZipFile(join(CUR_DIR, GEO_FILE_ZIP))
+    zip.extract(GEO_EXTRACT_DIR)
+
+def lookup_location(id):
+    locations = csv.reader(open(GEO_LOCATION_CSV, 'rb'), delimiter=',')
+    for row in locations:
+        if id == row[0]:
+            return row
+
+def lookup_country_name(country_code):
+    countries = csv.reader(open(COUNTRY_CODE_CSV, 'rb'), delimiter=';')
+    for row in countries:
+        if country_code.upper() == row[1]:
+            if row[1].upper() == "KR":
+                return "South Korea"
+            elif row[1].upper() == "KP":
+                return "North Korea"
+            
+            return row[0].split(',')[0].capitalize()
+
+def load_data():
+    print "Loading data..."
+    csv.field_size_limit(1000000000)
+    entries = csv.reader(open(GEO_BLOCK_CSV, 'rb'), delimiter='\t')
+    
+    num = 0
+    batch = []
+    print "Sending data..."
+    for row in entries:
+        print "Sending %s #%s..." % (row[2], num),
+        if len(batch) >= BATCH_SIZE:
+            print urllib.urlopen(SAVE_GEOIP_URL,
+                         urllib.urlencode(\
+                                      dict(geoip=json.dumps(batch)))).read()
+            batch=[]
+        
+        loc = lookup_location(row[2])
+        batch.append(dict(loc_id=row[2],
+                          start_number=row[0],
+                          end_number=row[1],
+                          country=lookup_country_name(loc[3]),
+                          country_code=loc[3],
+                          state_region=loc[4],
+                          city=loc[5],
+                          zipcode=loc[6],
+                          latitude=loc[7],
+                          longitude=loc[8]))
+        
+        num += 1
+    
+
+def load_cities():
+    pass
+
+if __name__ == "__main__":
+    download()
+    load_data()

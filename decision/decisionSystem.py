d6bc0cf5bf5e208a4cd4cb2eb5a97926ea793c85
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-10-21 23:31:04 +0100

    Fixed decision system

--- decision/decisionSystem.py
+++ decision/decisionSystem.py
@@ -33,70 +33,78 @@ class DecisionSystem:
         logging.info("Report received in decision system")
 
         event = Event()
-        event.firstDetectionUTC = datetime.datetime.now()
-        event.lastDetectionUTC  = datetime.datetime.now()
+        event.first_detection_utc = datetime.datetime.now()
+        event.last_detection_utc  = datetime.datetime.now()
 
         if isinstance(report, WebsiteReport):
             event.target = report.url
-            event.targetType = TargetType.Website
+            event.target_type = TargetType.Website
         elif isinstance(report, ServiceReport):
             event.target = report.serviceName
-            event.targetType = TargetType.Service
+            event.target_type = TargetType.Service
         else:
             raise TypeError, 'report not supported'
 
         event.active = True
 
         # TODO: get correct event type
-        event.eventType = EventType.Offline
-
-        # save event
-        event.save()
+        event.event_type = EventType.Offline
 
         # TODO: get isp information
         # TODO: save information about target server
 
         try:
+            event.location_ids.append(report.agent_location_id)
+            event.location_names.append(report.agent_location_name)
+            event.location_country_names.append(report.agent_country_name)
+            event.location_country_codes.append(report.agent_country_code)
+            event.lats.append(report.agent_lat)
+            event.lons.append(report.agent_lon)
 
-            # get blocked node from trace
-            if report.traceRoute:
-                ip = report.traceRoute.getBlockedNode().ip
-
-                # TODO: get geolocation information
-                blockedNode = EventBlockedNode()
-                blockedNode.ip = ip
-                blockedNode.city = "Unknown"
-                blockedNode.country = "Unknown"
-                blockedNode.latitude = random.randrange(-90,90,1)
-                blockedNode.longitude = random.randrange(0,180,1)
-                blockedNode.event = event
-                blockedNode.save()
-
-            # associate report to event
-            if isinstance(report, WebsiteReport):
-                eventReport = EventWebsiteReport(event=event, report=report)
-                eventReport.save()
-            elif isinstance(report, ServiceReport):
-                eventReport = EventServiceReport(event=event, report=report)
-                eventReport.save()
-
-            # save event location
-            # TODO: get location from agent ip
-            eventLocation = EventLocation()
-            eventLocation.city = "Unknown"
-            eventLocation.country = "Unknown"
-            eventLocation.latitude = random.randrange(-90,90,1)
-            eventLocation.longitude = random.randrange(0,180,1)
-            eventLocation.event = event
-            eventLocation.save()
-
+            # TODO: ISP!
+            event.isps.append('')
+            #isps = ListField()
+#
+#            # get blocked node from trace
+#            if report.trace:
+#                ip = report.trace.getBlockedNode().ip
+#
+#                # TODO: get geolocation information
+#                blockedNode = EventBlockedNode()
+#                blockedNode.ip = ip
+#                blockedNode.city = "Unknown"
+#                blockedNode.country = "Unknown"
+#                blockedNode.latitude = random.randrange(-90,90,1)
+#                blockedNode.longitude = random.randrange(0,180,1)
+#                blockedNode.event = event
+#                blockedNode.save()
+#
+#            # associate report to event
+#            if isinstance(report, WebsiteReport):
+#                eventReport = EventWebsiteReport(event=event, report=report)
+#                eventReport.save()
+#            elif isinstance(report, ServiceReport):
+#                eventReport = EventServiceReport(event=event, report=report)
+#                eventReport.save()
+#
+#            # save event location
+#            # TODO: get location from agent ip
+#            eventLocation = EventLocation()
+#            eventLocation.city = "Unknown"
+#            eventLocation.country = "Unknown"
+#            eventLocation.latitude = random.randrange(-90,90,1)
+#            eventLocation.longitude = random.randrange(0,180,1)
+#            eventLocation.event = event
+#            eventLocation.save()
+#
         except Exception,ex:
             logging.error(ex)
+
+        # save event
+        event.save()
 
         # report event no notification system
         NotificationSystem.publishEvent(event)
 
 
     newReport = staticmethod(newReport)
-
-  

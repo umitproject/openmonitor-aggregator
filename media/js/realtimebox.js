991428decc7dcabb2808c6cdc53d724f6544dbd4
Diogo Pinheiro <diogormpinheiro@gmail.com>
2011-10-22 02:56:24 +0100

    Fixed json encode Fixed realtimebox json parsing Fixed add events forms

--- media/js/realtimebox.js
+++ media/js/realtimebox.js
@@ -4,9 +4,9 @@ function addEventToList(event, appear)
     content = "<a href='" + event.url + "'>" + event.targetType + " " + event.type + " " + event.target + "</a><br />First Detection: " + formatDate(event.firstdetection) + "<br />Last Detection: " + formatDate(event.lastdetection) + "<br />Location: ";
     for(i=0; i<event.locations.length; i++)
     {
-        if(event.locations[i].city!="")
-            content += event.locations[i].city + ", ";
-        content += event.locations[i].country;
+        if(event.locations[i].location_name!="")
+            content += event.locations[i].location_name + ", ";
+        content += event.locations[i].location_country_name;
         if(i!=event.locations.length-1)
             content += "; ";
     }

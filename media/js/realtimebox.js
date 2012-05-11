fe70327c41394458d57d4f3a65c522f50f4b37cd
Orcun Avsar <orc.avs@gmail.com>
2012-05-12 02:23:11 +0300

    Render a template to show incoming events from Channel API.

--- media/js/realtimebox.js
+++ media/js/realtimebox.js
@@ -1,25 +1,72 @@
+var EVENT_SUMMARY_TEMPLATE = " \
+  <strong> {{ event_target_type }} {{ event_type }} </strong>\
+  <br />First Detection: {{ first_detection }}\
+  <br />Last Detection: {{ last_detection }}\
+  <br />Locations: {{ locations }}\
+";
+
+function receiveEvents(token, events){
+	var channel = new goog.appengine.Channel(token);
+    var handler = {
+        'onopen': onMapOpened,
+        'onmessage': onMapMessage,
+        'onerror': onMapError,
+        'onclose': function() {}
+    };
+    var socket = channel.open(handler);
+    socket.onopen = onMapOpened;
+    socket.onmessage = onMapMessage;
+    socket.onerror = onMapError;
+}
+
+function onMapOpened() {
+	$("#map_status").html("Waiting for events...")
+};
+
+function onMapMessage(m) {
+	event = JSON.parse(m.data);
+
+	addEventToMap(event, true);
+	addEventToList(event, true);
+}
+
+function onMapError(error) {
+	//alert(error);
+	//console.debug(error);
+	$("#map_status").html("Error loading events")
+}
+
 function addEventToList(event, appear)
 {
-    eventli = $( document.createElement('li') )
-    content = "<a href='" + event.url + "'>" + event.targetType + " " + event.type + " " + event.target + "</a><br />First Detection: " + formatDate(event.firstdetection) + "<br />Last Detection: " + formatDate(event.lastdetection) + "<br />Location: ";
+    event_div = $(document.createElement('div'))
+    locations = ""
     for(i=0; i<event.locations.length; i++)
     {
         if(event.locations[i].location_name!="")
-            content += event.locations[i].location_name + ", ";
-        content += event.locations[i].location_country_name;
+            locations += event.locations[i].location_name + ", ";
+        locations += event.locations[i].location_country_name;
         if(i!=event.locations.length-1)
-            content += "; ";
+            locations += "; ";
     }
-    eventli.addClass("event").html(content)
+    context = {
+    	      'event_url' : event.url,
+    	      'event_target_type': event.targetType,
+    	      'event_type': event.type,
+    	      'event_target': event.target,
+    	      'first_detection': formatDate(event.firstdetection),
+    	      'last_detection': formatDate(event.lastdetection),
+    	      'locations': locations,
+    	    };
+    
+    content_to_add = renderTemplate(EVENT_SUMMARY_TEMPLATE, context);
 
-    if(event.active)
-        eventli.addClass("active")
-    $("#realtime_ul").prepend(eventli)
-
-    if(appear)
-    {
-        eventli.hide()
-        eventli.slideDown("slow")
+    event_div.addClass("alert alert-error event-info");
+    event_div.html(content_to_add);
+    $("#events-box").prepend(event_div);
+    
+    if (appear) {
+    	event_div.hide();
+    	event_div.slideDown();
     }
 
     // TODO: format information; show all information

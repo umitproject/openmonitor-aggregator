c90f903e0c09800e05f199d6ba82c87826c430da
Adriano Marques <py.adriano@gmail.com>
2012-06-16 09:46:12 -0500

    uwsgi module

---
+++ aggregator_wsgi.py
@@ -0,0 +1,11 @@
+SITE_DIR = '/om'
+import site
+site.addsitedir(SITE_DIR) 
+
+import os
+import sys
+sys.path.append(SITE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()

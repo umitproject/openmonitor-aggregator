7af680d84789b654de94a5b4a4b7c2b589fd655b
Orcun Avsar <orc.avs@gmail.com>
2012-07-06 19:52:34 +0300

    Fix wrong import.

--- agents/CryptoLib.py
+++ agents/CryptoLib.py
@@ -27,7 +27,7 @@ import logging
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 from Crypto.Util.number import bytes_to_long
-from Crypto.Signature import PKCS1_v1_5
+from Crypto.Cipher import PKCS1_v1_5
 from Crypto.Hash import SHA
 
 from django.conf import settings
